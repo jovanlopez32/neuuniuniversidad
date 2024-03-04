15f541199b6d0a68b6409cc7adda51be02dfaac8
jovanlopez32 <everest.studio32@gmail.com>
2024-03-04 16:14:40 -0600

    Finish Tools Section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,26 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				nprimary: '#0047bb',
+				nsecondary: '#3ae9d6',
+				//Neuuni Colors
+				nyellow: '#ffb229',
+				nred: '#ff4a5c',
+				npurple: '#7748af',
+			},
+			animation: {
+				'gradient': 'gradient 8s linear infinite',
+			},
+			keyframes: {
+				'gradient': {
+					to: { 'background-position': '200% center' },
+				}
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('tailwindcss-animated')
+	],
 }
